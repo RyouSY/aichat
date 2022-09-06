6f639aad361bfd673c8c80793ae47ac8c17eb767
Ryou1127 <42777611+hatlsy@users.noreply.github.com>
2022-09-06 14:31:13 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+## 项目说明
+腾讯自然语言处理api实现的闲聊对话插件
+
+
+## 使用说明
+
+在 https://console.cloud.tencent.com/nlp/openconfirm 页面开通服务
+
+在 https://console.cloud.tencent.com/cam/capi 页面创建密钥
+
+把项目放到 `plugins`
+
+在 `aichat.js` 中填入你的 `SecretId` 和 `SecretKey`
+
+安装依赖 
+```
+pnpm add tencentcloud-sdk-nodejs-nlp -w
+```
+
+## 其他
+新手一枚，还有许多不完善的地方，请各位大佬多多包涵
---
+++ aichat.js
@@ -0,0 +1,43 @@
+import plugin from '../../lib/plugins/plugin.js'
+import tencentcloud from 'tencentcloud-sdk-nodejs-nlp'
+const NlpClient = tencentcloud.nlp.v20190408.Client;
+const clientConfig = {
+	credential: {
+		secretId: "",
+		secretKey: "",
+	},
+	region: "ap-guangzhou",
+	profile: {
+		signMethod: "HmacSHA256",
+		httpProfile: {
+			endpoint: "nlp.tencentcloudapi.com",
+		},
+	},
+};
+const client = new NlpClient(clientConfig);
+
+export class aiChat extends plugin {
+	constructor() {
+		super({
+			name: 'ai对话',
+			dsc: '人工智障',
+			event: 'message.group', //message为全部消息，message.group为群消息，message.private为私聊，message.discuss为讨论组
+			priority: 5000,
+			rule: [{
+				fnc: 'chatbot'
+			}]
+		})
+	}
+	chatbot(e) {
+		const params = {
+			"Query": e.msg
+		};
+		client.ChatBot(params).then(
+			(data) => {
+				this.reply(data.Reply)
+			},
+			(err) => {
+				logger.error(`error: ${err}`);
+			});
+	}
+}
